c47e0ab9d01b348d802fe46106218d9ed72752dc
Benjamin Connelly <bconnelly@prowd.local>
2017-08-08 11:29:43 -0500

    link.sh

---
+++ link.sh
@@ -0,0 +1,5 @@
+ln -s ~/.dotfiles/.vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/.vim/ ~/.vim
+ln -s ~/.dotfiles/.bash/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.bash/.bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/.bash/.profile ~/.profile
